c3ff8504f02f637f992acbb7b66da242c73ded36
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-04-27 10:36:47 -0300

    Updates

---
+++ precache-manifest.d2034de73916f9c4057bce04ea4e5f16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39694723db5095ed0cb06365b2103db1",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "e2ce25e683246cb9bd47",
+    "url": "/todo-app/static/js/2.e8a984b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-app/static/js/2.e8a984b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f57d5ec54dc96265ae49",
+    "url": "/todo-app/static/js/main.f32d19ab.chunk.js"
+  },
+  {
+    "revision": "dcc5182979e8de7657cb",
+    "url": "/todo-app/static/js/runtime-main.7427f49f.js"
+  }
+]);
