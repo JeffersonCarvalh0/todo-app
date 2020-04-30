5c0fbf84328445a7b3c6fb369ff01cc54c128138
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-04-30 16:43:08 -0300

    feat(auth): use getUserRepository function and checkPassword method

--- packages/server/src/auth.ts
+++ packages/server/src/auth.ts
@@ -3,7 +3,7 @@ import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
 import { Strategy as LocalStrategy } from 'passport-local';
 import bcrypt from 'bcrypt';
 
-import User, { UserRepository } from './entity/User';
+import User, { getUserRepository } from './entity/User';
 
 export const hashPassword = async (plainTextPassword: string) => {
   const hashedPassword = await bcrypt.hash(plainTextPassword, 12);
@@ -12,25 +12,17 @@ export const hashPassword = async (plainTextPassword: string) => {
 
 // Set local strategy for authentication with username/password
 Passport.use(
-  new LocalStrategy((email, plainTextPassword, done) => {
-    UserRepository.findOne({ email })
+  new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    getUserRepository()
+      .findOne({ email })
       .then((user: User | null) => {
         if (!user) {
           return done(null, false, { message: 'Invalid email' });
         }
-        bcrypt.compare(
-          plainTextPassword,
-          user.password,
-          (err: Error, isMatch: boolean) => {
-            if (err) {
-              throw err;
-            }
-            if (!isMatch) {
-              return done(null, false, { message: 'Invalid password' });
-            }
-            return done(null, user, { message: 'Successfully authenticated' });
-          },
-        );
+
+        return user.checkPassword(password)
+          ? done(null, user, { message: 'Successfully authenticated' })
+          : done(null, false, { message: 'Invalid password' });
       })
       .catch((err: Error) => done(err));
   }),
@@ -43,7 +35,8 @@ const opts = {
 };
 Passport.use(
   new JwtStrategy(opts, (jwtPayload, done) => {
-    UserRepository.findOne({ name: jwtPayload.name })
+    getUserRepository()
+      .findOne({ name: jwtPayload.name })
       .then((user: User | null) => {
         if (!user) {
           return done(null, false, {
