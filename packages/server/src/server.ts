535dc836c0d6dd5eb009d1531c99ef65e001196c
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-04-30 16:45:46 -0300

    fix(server): fix dotenv data loading

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,13 +1,12 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import { createConnection } from 'typeorm';
-import dotenv from 'dotenv';
 
 import runApp from './app';
 
 /** Database connection */
 createConnection()
   .then(async () => {
-    dotenv.load();
     const port = process.env.PORT || 3000;
     runApp(port);
     console.info(`Listening to http://localhost:${port} 🚀`);
