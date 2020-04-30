01eca3f6f7b34646939d5ef7502a751508d1f553
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-04-29 21:41:06 -0300

    feat(server): load dotenv file

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+import dotenv from 'dotenv';
 
 import runApp from './app';
 
 /** Database connection */
 createConnection()
-  .then(async (connection) => {
+  .then(async () => {
+    dotenv.load();
     const port = process.env.PORT || 3000;
-    runApp(connection, port);
+    runApp(port);
     console.info(`Listening to http://localhost:${port} 🚀`);
   })
   .catch((error) => console.log('TypeORM connection error: ', error));
