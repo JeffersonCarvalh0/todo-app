b8e73557b27231319ed557b7918f97256da3f46e
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-05-10 11:31:46 -0300

    feat(storybook): add globalStyle and theme to all stories

--- packages/web/.storybook/config.ts
+++ packages/web/.storybook/config.ts
@@ -1,4 +1,5 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import themeDecorator from './themeDecorator';
 
 const req = require.context("../src", true, /\.story\.tsx$/);
 function loadStories() {
@@ -6,3 +7,4 @@ function loadStories() {
 }
 
 configure(loadStories, module);
+addDecorator(themeDecorator);
