956fde76ad31fb6e68c227d1d394a28b15943f02
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-05-11 10:20:19 -0300

    feat(button): add marginTop parameter

--- packages/web/src/components/Button/Button.tsx
+++ packages/web/src/components/Button/Button.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
+interface StyledProps {
+  marginTop?: string;
+}
+
 const StyledButton = styled.button`
   background-color: ${(props) => props.theme.colors.accent};
   border: none;
   border-radius: 50px;
   padding: 10px 20px;
   transition: opacity 250ms ease;
+  margin-top: ${(props: StyledProps) => props.marginTop || '0px'};
 
   &:focus {
     outline: none;
@@ -22,8 +27,12 @@ interface Props {
   children: React.ReactNode;
 }
 
-const Button = ({ onClick, children }: Props) => {
-  return <StyledButton onClick={onClick}>{children}</StyledButton>;
+const Button = ({ onClick, children, marginTop }: Props & StyledProps) => {
+  return (
+    <StyledButton onClick={onClick} marginTop={marginTop}>
+      {children}
+    </StyledButton>
+  );
 };
 
 export default Button;
