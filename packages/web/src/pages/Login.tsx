5e1edd52cd2795260b8b12e5ce5fdd9aaf92c7e3
Jefferson Carvalho <jeffersonhcarvalho@gmail.com>
2020-05-12 11:56:14 -0300

    feat(login): add api call, loading overlay and error messages to login

--- packages/web/src/pages/Login.tsx
+++ packages/web/src/pages/Login.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Formik, Form, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 
 import TextInput from '../components/TextInput';
+import ErrorMessage from '../components/ErrorMessage';
 import Button from '../components/Button';
+import FullLoading from '../components/FullLoading';
+import server from '../api';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  transition: all 250ms ease;
+`;
+
+const ApiErrorMessage = styled.h3`
+  color: ${(props) => props.theme.colors.error};
+  text-align: center;
 `;
 
 const Login = () => {
+  const [token, setToken] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
   return (
     <Formik
       initialValues={{ Email: '', Password: '' }}
@@ -19,20 +31,48 @@ const Login = () => {
         Email: Yup.string().required().email(),
         Password: Yup.string().required(),
       })}
-      onSubmit={() => {}}
+      onSubmit={(values, { setSubmitting }) => {
+        server
+          .post('/login', {
+            email: values.Email,
+            password: values.Password,
+          })
+          .then((response) => {
+            if (response) {
+              if (response.data.data.token) setToken(response.data.data.token);
+            }
+          })
+          .catch((error) => {
+            if (error) {
+              setErrorMessage(error.response.data.message);
+            }
+          })
+          .then(() => {
+            setSubmitting(false);
+          });
+      }}
     >
-      <Form>
-        <Container>
-          <TextInput label="Email" />
-          <ErrorMessage name="Email" />
-          <TextInput label="Password" obscure />
-          <ErrorMessage name="Password" />
-          <Button type="submit" marginTop="20px">
-            Login
-          </Button>
-          <Button marginTop="10px"> Create a new account </Button>
-        </Container>
-      </Form>
+      {(formik) => (
+        <Form>
+          <FullLoading show={formik.isSubmitting} />
+          <Container>
+            {token ? <h1>{token}</h1> : <></>}
+            {errorMessage ? (
+              <ApiErrorMessage>{errorMessage}</ApiErrorMessage>
+            ) : (
+              <></>
+            )}
+            <TextInput label="Email" />
+            <ErrorMessage name="Email" />
+            <TextInput label="Password" obscure />
+            <ErrorMessage name="Password" />
+            <Button type="submit" marginTop="20px">
+              Login
+            </Button>
+            <Button marginTop="10px"> Create a new account </Button>
+          </Container>
+        </Form>
+      )}
     </Formik>
   );
 };
